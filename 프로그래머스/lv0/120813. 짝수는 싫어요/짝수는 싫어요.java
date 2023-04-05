13180a858fa15c795dcbe552dc53921b7dca9f4a
eunbiro <115062953+eunbiro@users.noreply.github.com>
2023-04-05 20:33:26 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.03 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Integer> solution(int n) {
+            List<Integer> answer = new ArrayList<Integer>();
+
+            for (int i = 1; i < n + 1; i++) {
+                if (i % 2 != 0) {
+                    answer.add(i);
+                }
+            }
+
+        return answer;
+    }
+}
